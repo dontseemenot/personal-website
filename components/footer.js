301df5a68693b56442c78fde8c511aa1ece4d08e
arthur <artsze10@gmail.com>
2022-11-18 01:21:18 +1100

    Work exp page, header, footer

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="contact">
+                <ul>
+                    <li>arthur_sze@outlook.com</li>
+                    <li><a href="https://www.linkedin.com/in/arthur-sze/">
+                        <img class="icon" src="images/linkedin.svg" alt="linkedin" />
+                    </a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                <p>Made with plain HTML, CSS, and JS. © 2022 Arthur Sze.</p>
+            </div>
+        `;
+    }
+}
+
+customElements.define('footer-component', Footer);
---
+++ components/navbar.js
@@ -0,0 +1,28 @@
+// var title = document.querySelector(".top-container h1");
+// title.innerHTML = "Hi, I'm Arthur, a Software Engineer.";
+
+// var subtitles = document.querySelectorAll(".top-container h2");
+// console.log(subtitles);
+
+// var content = document.querySelector('link[rel="import"]').import;
+
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <ul class="left">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="work.html">Work Experience</a></li>
+            </ul>
+            <ul class="right">
+            </ul>
+
+        `;
+
+    }
+}
+
+customElements.define('navbar-component', NavBar);
