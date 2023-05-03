ce8cf1bd1ce724a654d350f085dd9d7ca2a7a385
arthur <artsze10@gmail.com>
2023-05-03 23:20:23 +1000

    Overhauled website front page

--- components/footer.js
+++ components/footer.js
@@ -8,9 +8,6 @@ class Footer extends HTMLElement {
             <div class="contact">
                 <ul>
                     <li>arthur_sze@outlook.com</li>
-                    <li><a href="https://www.linkedin.com/in/arthur-sze/">
-                        <img class="icon" src="images/linkedin.svg" alt="linkedin" />
-                    </a></li>
                 </ul>
             </div>
             <div class="copyright">
--- components/navbar.js
+++ components/navbar.js
@@ -15,9 +15,49 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
             <nav>
                 <ul class="left">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="work.html">Work Experience</a></li>
-                    <li><a href="thesis.html">Thesis</a></li>
+                
+                    <li class="nav-button"><a href="/">Home</a></li>
+
+                    <li class="nav-button">Work Experience &#x25BE
+
+                        <ul class="dropdown">
+
+                            <li>Commonwealth Bank of Australia &#x25BE</li>
+                            <ul class="subdropdown">
+                                <li class="highlightable"><a href="#">Backend Software Engineer</li>
+                                <li class="highlightable"><a href="#">Graduate Backend Software Engineer</li>
+                                <li class="highlightable"><a href="#">Graduate Site Reliability Engineer</li>
+                            </ul>
+                            
+
+                            <li>UNSW &#x25BE</li>
+                                <ul class="subdropdown">
+                                    <li class="highlightable"><a href="#">Research Assistant</li>
+                                    <li class="highlightable"><a href="#">Student Developer</li>
+                                </ul>
+
+                            
+                            <li>ResMed &#x25BE</li>
+                                <ul class="subdropdown">
+                                    <li class="highlightable"><a href="#">Electrical Engineering Intern</li>
+                                </ul>
+
+
+                            <li class="highlightable"><a href="#">Casual</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-button">Projects &#x25BE
+
+                    <ul class="dropdown">
+
+                        <li class="highlightable"><a href="thesis.html">Thesis</a></li>
+                        
+                    </ul>
+                </li>
+                    
+
+
                 </ul>
                 <ul class="right">
 
