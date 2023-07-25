b3e25952f462c5fe5c2866c6f4bfba23c0145e6e
arthur <artsze10@gmail.com>
2023-07-26 00:21:57 +1000

    v2.0

--- components/navbar.js
+++ components/navbar.js
@@ -8,22 +8,18 @@ class NavBar extends HTMLElement {
         
             <div class="left">
 
-                <div class="category">
-                    <a href="/"><div class="title">Home</div></a>
-                </div>
+            <div class="category">
+                <a href="/"><div class="title">Home</div></a>
+            </div>
+            <div class="category">
+                <a href="/work.html"><div class="title">Work Experience</div></a>
+            </div>
+            <div class="category">
+                <a href="/projects.html"><div class="title">Projects</div></a>
+            </div>
                 
-                <div class="category work-exp">
 
-                    <div class="title with-dropdown">Work Experience &#x25BE</div>
-                    <div class="dropdown">
-                        <a href="/"><div class="link">CBA - Backend Software Engineer</div></a>
-                        <a href="/"><div class="link">CBA - Graduate Backend Software Engineer</div></a>
-                        <a href="/"><div class="link">CBA - Graduate Site Reliability Engineer</div></a>
-                        <a href="/"><div class="link">UNSW - Research Assistant</div></a>
-                        <a href="/"><div class="link">ResMed - Electrical Engineering Intern</div></a>
-                        <a href="/"><div class="link">UNSW - Student Developer</div></a>
-                    </div>
-                </div>
+                
             </div>
     
             <div class="right">
